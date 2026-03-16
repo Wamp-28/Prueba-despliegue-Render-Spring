59fe05099f0ce8355d8084d2590b32bb99cc7c30
AlexM <wmatallana28@gmail.com>
2026-03-15 20:07:02 -0500

    Cambios

--- src/main/java/com/example/Relaciones/Configuration/Swagger.java
+++ src/main/java/com/example/Relaciones/Configuration/Swagger.java
@@ -1,10 +1,14 @@
 package com.example.Relaciones.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Swagger {
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
